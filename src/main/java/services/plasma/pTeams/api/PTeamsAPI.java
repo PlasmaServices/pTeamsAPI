71ada0210a31fc3d3ab1394a94301d26c00eb976
herberto <herberto@herberto.xyz>
2026-01-29 20:43:31 -0800

    Initial commit

---
+++ src/main/java/services/plasma/pTeams/api/PTeamsAPI.java
@@ -0,0 +1,17 @@
+package services.plasma.pTeamsAPI;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PTeamsAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
